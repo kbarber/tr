4d6009cb1d3b07702a733f3c3e5cb818a06e16bd
Craig Knox <knoxc@alfresco.com>
2009-04-20 14:13:42 +0100

    Move system methods out into TR::C::System.
    --HG--
    extra : convert_revision : 6358a0bf05c856a66ba31fb90d2b1d8f25a30f16

--- lib/TR.pm
+++ lib/TR.pm
@@ -120,7 +120,7 @@ sub forward {
 
   # This preloads the controllers so we know what paths are handled
   # plus gives us our default controller.
-  my $controller = $self->_get_controller(type => 'TR::C::System');
+  my $default = $self->_get_controller(type => 'TR::C::System');
 
   if (my $handler = $handlers_by_path->{$path}) {
     $self->_run_method($args{'method'},
@@ -128,7 +128,9 @@ sub forward {
                        context   => $self->context);
   }
   else {
-    $self->_run_method($args{'method'}, context => $self->context);
+    $self->_run_method($args{'method'},
+                       'package' => ref($default),
+                       context   => $self->context);
   }
 
   return;
@@ -222,154 +224,6 @@ sub _run_method {
 
   E::Invalid::Method->throw(error    => 'No method given',
                             err_code => '-32601' );
-}
-
-=head2 system_version
-
-  Request:
-    time curl -H 'Content-Type: application/json' -u test:test -X POST -d '
-    {
-      "jsonrpc":"2.0",
-      "method":"system.version",
-    }
-    ' 'http://tr.test.alfresco.com/t/ldap/user'
-
-  Response:
-    {
-      "jsonrpc":"2.0",
-      "result": {"version":"x.x"} 
-    }
-
-=cut
-sub system_version :Global {
-  my $self = shift;
-
-  $self->context->result({version => $self->version});
-
-  return;
-}
-
-=head2 system_doc
-
-  Request:
-    time curl -H 'Content-Type: application/json' -u test:test -X POST -d '
-    {
-      "jsonrpc":"2.0",
-      "method":"system.doc",
-      "params":{
-        "show":"getDnByUid"
-      }
-    }
-    ' 'http://tr.test.alfresco.com/t/ldap/user'
-
-  Response:
-    {
-      "jsonrpc":"2.0",
-      "result": {"doc":" ...documentation... "} 
-    }
-
-=cut
-sub system_doc :Global {
-=begin schema
-  {
-    "type": "map",
-    "required": false,
-    "mapping": {
-      "show":   { "type": "str", "required": false }
-    }
-  }
-=cut
-
-  my $self = shift;
-
-  $self->context->result({doc => {version => $self->version}});
-
-  # See if documentation for a specific method was wanted
-  if ($self->context) {
-    if (my $params = $self->context->params()) {
-      if ($self->_is_public_method($params->{'show'})) {
-        my $pod = new TR::Pod;
-
-        if (my $doc = $pod->get_documentation('package' => ref($self),
-                                              method    => $params->{'show'})) {
-          $self->context->result({doc => {method => $params->{'show'}}});
-          $self->context->result({doc => {poddoc => $doc}});
-          return;
-        }
-      }
-    }
-  }
-
-  # Else display list of available methods
-  my %result;
-  my $handlers = $self->_get_handler_paths();
-  use Data::Dumper;
-  warn Dumper $handlers;
-  foreach my $path (keys %{$handlers}) {
-    my $methods = $handlers->{$path}{'methods'};
-    my $package = $handlers->{$path}{'package'};
-    if ($path eq '') {
-      $path = 'GLOBAL';
-    }
-    $result{$path} = $methods;
-  }
-  $self->context->result({doc => {paths => \%result}});
-}
-
-=head2 system_schema
-
-  Request:
-    time curl -H 'Content-Type: application/json' -u test:test -X POST -d '
-    {
-      "jsonrpc":"2.0",
-      "method":"system.schema",
-      "params":{
-        "show":"doc"
-      }
-    }
-    ' 'http://tr.test.alfresco.com/t/'
-
-  Response:
-    {
-      "jsonrpc":"2.0",
-      "result": {"schema": " ...Kwalify schema... "} 
-    }
-
-=cut
-sub system_schema :Global {
-=begin schema
-  {
-    "type": "map",
-    "required": true,
-    "mapping": {
-      "show":   { "type": "str", "required": true }
-    }
-  }
-=cut
-
-  my $self = shift;
-
-  $self->context->result(doc => {version => $self->version});
-
-  if ($self->context) {
-    if (my $params = $self->context->params()) {
-      if ($self->_is_public_method($params->{'show'})) {
-        $self->context->result(doc => {method => $params->{'show'}});
-
-        my $pod = new TR::Pod;
-        if (my $schema = $pod->get_schema('package' => ref($self),
-                                          method    => $params->{'show'})) {
-          $self->context->result(doc => {schema => $schema});
-        }
-        else {
-          $self->context->result(doc => {schema => 'No schema.'});
-        }
-        return;
-      }
-    }
-  }
-
-  return;
 }
 
 =head2 _error_handler
--- lib/TR/Controller.pm
+++ lib/TR/Controller.pm
@@ -1,13 +1,16 @@
 package TR::Controller;
 use TR::Global;
 use base 'TR::Attributes';
-__PACKAGE__->mk_ro_accessors(qw/context config/);
+__PACKAGE__->mk_ro_accessors(qw/context config version/);
+
+my $VERSION = '0.04';
 
 sub new {
   my ($proto, %args) = @_;
   my ($class) = ref $proto || $proto;
 
   my $self = bless \%args, $class;
+  $self->{'version'} = $VERSION;
   $self->_init();
 
   return $self;
