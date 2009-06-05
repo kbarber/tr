ff732fc6d00f2afea613b7803a382159c50b9adc
Craig Knox <knoxc@alfresco.com>
2009-06-05 11:59:22 +0100

    Code cleanups
    --HG--
    extra : convert_revision : 3f98af05050d3b860a209e90276e76972e4d4f28

--- lib/TR.pm
+++ lib/TR.pm
@@ -1,6 +1,6 @@
 package TR;
 use TR::Standard;
-use English;
+use English qw(-no_match_vars);
 
 use vars qw($VERSION);
 use version; $VERSION = qv('1.0');
@@ -8,6 +8,10 @@ use version; $VERSION = qv('1.0');
 =head1 NAME
 
   TR - Engine
+
+=head1 VERSION
+
+  1.0
 
 =head1 LICENSE AND COPYRIGHT
 
@@ -35,26 +39,48 @@ use version; $VERSION = qv('1.0');
 
   See <TR::C::System> for an example of a control module.
 
+=head1 CONFIGURATION AND ENVIRONMENT
+
+  Can be run either as a script:
+  my $cgi = new CGI::Simple();
+  my $app = new TR(config => 'config.file', request => $cgi);
+  $app->handler();
+
+  Or mod_perl: See SYNOPSIS
+
+=head1 DEPENDENCIES
+
+=head1 INCOMPATIBILITIES
+
+=head1 AUTHOR
+
+=head1 DIAGNOSTICS
+
+=head1 BUGS AND LIMITATIONS
+
+  Probably a few.
+
 =head1 SUBROUTINES/METHODS
 
 =cut
 
-use Module::Pluggable search_path => 'TR::Context',
-                      inner       => 0,
-                      sub_name    => 'context_handlers',
-                      instantiate => 'new';
+use Module::Pluggable
+    search_path => 'TR::Context',
+    inner       => 0,
+    sub_name    => 'context_handlers',
+    instantiate => 'new';
 
-use Module::Pluggable search_path => 'TR::Plugins',
-                      inner       => 0,
-                      sub_name    => 'plugins',
-                      instantiate => 'new';
+use Module::Pluggable
+    search_path => 'TR::Plugins',
+    inner       => 0,
+    sub_name    => 'plugins',
+    instantiate => 'new';
 
-use Module::Pluggable search_path => 'TR::C',
-                      inner       => 0,
-                      sub_name    => 'controllers',
-                      instantiate => 'new';
-
-# TODO Move error handling to context.
+use Module::Pluggable
+    search_path => 'TR::C',
+    inner       => 0,
+    sub_name    => 'controllers',
+    instantiate => 'new';
 
 use attributes;
 
@@ -66,13 +92,17 @@ use TR::Exceptions;
 use TR::Request;
 
 use base 'TR::Attributes';
-__PACKAGE__->mk_ro_accessors(qw/request
-                                config/);
+__PACKAGE__->mk_ro_accessors(
+    qw/request
+        config/
+);
 
-__PACKAGE__->mk_accessors(qw/debug
-                             context
-                             version
-                             log/);
+__PACKAGE__->mk_accessors(
+    qw/debug
+        context
+        version
+        log/
+);
 
 =head2 new
 
@@ -85,37 +115,38 @@ __PACKAGE__->mk_accessors(qw/debug
     PACKAGE->new(request => $apache_request_object);
 
 =cut
+
 sub new {
-  my ($proto, %args) = @_;
-  my ($class) = ref $proto || $proto;
+    my ( $proto, %args ) = @_;
+    my ($class) = ref $proto || $proto;
 
-  my $request = new TR::Request(%args);
+    my $request = new TR::Request(%args);
 
-  my $self = bless {
-               version => $VERSION,
-             }, $class;
+    my $self = bless { version => $VERSION, }, $class;
 
-  if ($args{'config'}) {
-    if (-f $args{'config'}) {
-      $self->{'config'} = new TR::Config($args{'config'});
+    if ( $args{'config'} ) {
+        if ( -f $args{'config'} ) {
+            $self->{'config'} = new TR::Config( $args{'config'} );
+        }
+        else {
+            E::Invalid::Config->throw(
+                "Couldn't find config file: $args{'config'}");
+        }
+
+        # Try and see if logging is configured.
+        if ( my $log_config = $self->config->{'log'} ) {
+            Log::Log4perl->init_once( $log_config->{'conf'} );
+            if ( my $logger = Log::Log4perl->get_logger() ) {
+                $self->log($logger);
+            }
+        }
     }
-    else {
-      E::Invalid::Config->throw("Couldn't find config file: $args{'config'}");
-    }
 
-    # Try and see if logging is configured.
-    if (my $log_config = $self->config->{'log'}) {
-      Log::Log4perl->init_once($log_config->{'conf'});
-      if (my $logger = Log::Log4perl->get_logger()) {
-        $self->log($logger);
-      }
-    }
-  }
-
-  # Default logging setup.
-  if (not $self->log) {
-    ## no critic
-    Log::Log4perl->init(\qq{
+    # Default logging setup.
+    if ( not $self->log ) {
+        ## no critic
+        Log::Log4perl->init(
+            \qq{
       log4perl.rootLogger=ERROR, LOGFILE   
 
       log4perl.appender.LOGFILE=Log::Log4perl::Appender::ScreenColoredLevels
@@ -123,63 +154,65 @@ sub new {
     
       log4perl.appender.LOGFILE.layout=PatternLayout
       log4perl.appender.LOGFILE.layout.ConversionPattern=[%d] %m%n
-      });
-    if (my $logger = Log::Log4perl->get_logger()) {
-      $self->log($logger);
-    }
-  }
-
-  eval {
-    foreach my $context ($self->context_handlers()) {
-      next unless $context->can('handles');
-      if ($context->handles(request => $request)) {
-        $self->context($context);
-        eval {
-          $context->init();
-          1;
+      }
+        );
+        if ( my $logger = Log::Log4perl->get_logger() ) {
+            $self->log($logger);
         }
-        or do {
-          $self->_error_handler($EVAL_ERROR);
-        };
-        last;
-      };
     }
 
-    if (not $self->context()) {
-      E::Invalid::ContentType->throw("Don't know how to handle: " .
-                                     $request->content_type());
+    eval {
+        foreach my $context ( $self->context_handlers() ) {
+            next if not $context->can('handles');
+            if ( $context->handles( request => $request ) ) {
+                $self->context($context);
+                eval {
+                    $context->init();
+                    1;
+                }
+                or do {
+                    $self->_error_handler($EVAL_ERROR);
+                };
+                last;
+            }
+        }
+
+        if ( not $self->context() ) {
+            E::Invalid::ContentType->throw(
+                'Don\'t know how to handle: ' . $request->content_type() );
+        }
+        1;
     }
-    1
-  }
-  or do {
-    $self->_error_handler($EVAL_ERROR);
-  };
+    or do {
+        $self->_error_handler($EVAL_ERROR);
+    };
 
-  # This preloads the controllers so we know what paths are handled
-  $self->_get_controller(type => 'Whoknows');
+    # This preloads the controllers so we know what paths are handled
+    $self->_get_controller( type => 'Whoknows' );
 
-  return $self;
+    return $self;
 }
 
 =head2 handler
 
 =cut
+
 sub handler {
-  my $self = shift;
-  
-  eval {
-    $self->forward($self->context->request->rpc_path());
-    1;
-  }
-  or do {
-    $self->_error_handler($EVAL_ERROR);
-  };
+    my $self = shift;
 
-  if ($self->context) {
-    $self->context->view();
-  }
+    eval {
+        $self->forward( $self->context->request->rpc_path() );
+        1;
+        }
+        or do {
+        $self->_error_handler($EVAL_ERROR);
+        };
 
-  return;
+    if ( $self->context ) {
+        $self->context->view();
+    }
+
+    return;
 }
 
 =head2 forward
@@ -188,23 +221,28 @@ sub handler {
   module to handle.
 
 =cut
+
 sub forward {
-  my ($self, $path, %args) = @_;
+    my ( $self, $path, %args ) = @_;
 
-  my $handlers_by_path = $self->_get_handler_paths;
+    my $handlers_by_path = $self->_get_handler_paths;
 
-  if (my $handler = $handlers_by_path->{$path}) {
-    $self->_run_method($args{'method'},
-                       'package' => $handler->{'package'},
-                       context   => $self->context);
-  }
-  else {
-    $self->_run_method($args{'method'},
-                       'package' => 'TR::C::System',
-                       context   => $self->context);
-  }
+    if ( my $handler = $handlers_by_path->{$path} ) {
+        $self->_run_method(
+            $args{'method'},
+            'package' => $handler->{'package'},
+            context   => $self->context
+        );
+    }
+    else {
+        $self->_run_method(
+            $args{'method'},
+            'package' => 'TR::C::System',
+            context   => $self->context
+        );
+    }
 
-  return;
+    return;
 }
 
 =head2
@@ -212,21 +250,24 @@ sub forward {
   Returns a controller matching given type
 
 =cut
+
 sub _get_controller {
-  my ($self, %args) = @_;
+    my ( $self, %args ) = @_;
 
-  return if not $args{'type'};
+    return if not $args{'type'};
 
-  my @controllers = $self->controllers(context => $self->context,
-                                       config  => $self->config);
+    my @controllers = $self->controllers(
+        context => $self->context,
+        config  => $self->config
+    );
 
-  foreach my $controller (@controllers) {
-    if (ref($controller) eq $args{'type'}) {
-      return $controller;
+    foreach my $controller (@controllers) {
+        if ( ref($controller) eq $args{'type'} ) {
+            return $controller;
+        }
     }
-  }
 
-  return;
+    return;
 }
 
 =head2 _run_method 
@@ -236,69 +277,75 @@ sub _get_controller {
   Checks that it is a public method.
 
 =cut
+
 sub _run_method {
-  my ($self, $method, %args) = @_;
+    my ( $self, $method, %args ) = @_;
 
-  if (not $method) {
-    $method = $args{'context'}->method();
-  }
-
-  if ($method) {
-    $method =~ s/\./_/x;
-
-    my $control; 
-    
-    if (not $control = $self->_get_controller(type => $args{'package'})) {
-      $control = $self; # For now
+    if ( not $method ) {
+        $method = $args{'context'}->method();
     }
 
-    if ($control->_is_public_method($method)) {
-      # Hook to allow a plugins to run before a method has been called.
-      foreach my $plugin ($self->plugins) {
-        next unless $plugin->can('pre_method_hook');
-        $plugin->pre_method_hook(control => $control,
-                                 method  => $method);
-      }
+    if ($method) {
+        $method =~ s/\./_/xsm;
 
-      # Run the method
-      eval {
-        $control->$method();
-        1;
-      }
-      or do {
-        # Not sure about this way of redirecting between controllers...
-        # Seems wrong to raise an error to cause a redirect,
-        # but it was a quick fix till a nice way is done.. 
-        # maybe via attributes? sub createUser :Alias(/ldap/user)
-        my $e;
-        if ($e = Exception::Class->caught('E::Redirect')) {
-          my $method = $e->method || $self->context->method();
-          $self->forward($e->newpath, method => $method);
+        my $control = $self->_get_controller( type => $args{'package'} );
+
+        if ( $control->_is_public_method($method) ) {
+
+            # Hook to allow a plugins to run before a method has been called.
+            foreach my $plugin ( $self->plugins ) {
+                next if not $plugin->can('pre_method_hook');
+                $plugin->pre_method_hook(
+                    control => $control,
+                    method  => $method
+                );
+            }
+
+            # Run the method
+            eval {
+                $control->$method();
+                1;
+            }
+            or do {
+
+               # Not sure about this way of redirecting between controllers...
+               # Seems wrong to raise an error to cause a redirect,
+               # but it was a quick fix till a nice way is done..
+               # maybe via attributes? sub createUser :Alias(/ldap/user)
+                my $e;
+                if ( $e = Exception::Class->caught('E::Redirect') ) {
+                    my $method = $e->method || $self->context->method();
+                    $self->forward( $e->newpath, method => $method );
+                }
+                elsif ( $e = Exception::Class->caught() ) {
+                    ref $e ? $e->rethrow : E::Fatal->throw($e);
+                }
+            };
+
+            # Hook to allow a plugins to run after a method has been called.
+            foreach my $plugin ( $self->plugins ) {
+                next if not $plugin->can('post_method_hook');
+                $plugin->post_method_hook(
+                    control => $control,
+                    method  => $method
+                );
+            }
+            return;
         }
-        elsif ($e = Exception::Class->caught())  {
-          ref $e ? $e->rethrow : E::Fatal->throw($e);
+        else {
+            E::Invalid::Method->throw(
+                error    => $method,
+                err_code => '-32601'
+            );
         }
-      };
-
-
-      # Hook to allow a plugins to run after a method has been called.
-      foreach my $plugin ($self->plugins) {
-        next unless $plugin->can('post_method_hook');
-        $plugin->post_method_hook(control => $control,
-                                  method  => $method);
-      }
-      return;
     }
-    else {
-      E::Invalid::Method->throw(error    => $method,
-                                err_code => '-32601' );
-    }
-  }
 
-  E::Invalid::Method->throw(error    => 'No method given',
-                            err_code => '-32601' );
+    E::Invalid::Method->throw(
+        error    => 'No method given',
+        err_code => '-32601'
+    );
 
-  return;  # Should never get here.
+    return;    # Should never get here.
 }
 
 =head2 _error_handler
@@ -306,33 +353,30 @@ sub _run_method {
   Handle errors in module.
 
 =cut
+
 sub _error_handler {
-  my ($self, $exception) = @_;
+    my ( $self, $exception ) = @_;
 
-  if (ref($exception)) {
-    my %error;
-    $error{'message'} = $exception->description() .
-                        ': ' .
-                        $exception->error;
+    if ( ref $exception ) {
+        my %error;
+        $error{'message'}
+            = $exception->description() . ': ' . $exception->error;
 
-    $self->log->error($error{'message'});
+        $self->log->error( $error{'message'} );
 
-    $self->log->debug($exception->description() .
-                      ' : ' .
-                      $exception->error() .
-                      ' : ' .
-                      $exception->trace->as_string);
+        $self->log->debug( $exception->description() . ' : '
+                . $exception->error() . ' : '
+                . $exception->trace->as_string );
 
+        $error{'err_code'} = $exception->err_code();
+        $self->context->result( { error => \%error } );
+    }
+    else {
+        $self->context->result( { error => 'Unknown error' } );
+        $self->log->error("Unknown error: $exception");
+    }
 
-    $error{'err_code'} = $exception->err_code();
-    $self->context->result({error => \%error});
-  }
-  else {
-    $self->context->result({error => "Unknown error"});
-    $self->log->error("Unknown error: $exception");
-  }
-
-  return;
+    return;
 }
 
 1;
